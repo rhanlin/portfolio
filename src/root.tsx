3df6e0c7aad70e61382ca2213c1d561f01b9ac46
Rhanlin <spencerlin999@gmail.com>
2025-02-06 13:54:19 +0800

    feat: lint setting

--- src/root.tsx
+++ src/root.tsx
@@ -1,13 +1,13 @@
-import { component$ } from "@builder.io/qwik";
+import { component$ } from '@builder.io/qwik';
 import {
   QwikCityProvider,
   RouterOutlet,
   ServiceWorkerRegister,
-} from "@builder.io/qwik-city";
-import { RouterHead } from "./components/router-head/router-head";
-import { isDev } from "@builder.io/qwik";
+} from '@builder.io/qwik-city';
+import { RouterHead } from './components/router-head/router-head';
+import { isDev } from '@builder.io/qwik';
 
-import "./global.css";
+import './global.css';
 
 export default component$(() => {
   /**
